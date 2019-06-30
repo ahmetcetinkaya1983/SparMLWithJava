637a262a2af9ee2f6613d2449b720b43b7b41eba
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-06-30 19:12:38 +0300

    training, test and holdout data

--- src/main/java/com/virtualpairprogrammers/HousePriceAnalysis.java
+++ src/main/java/com/virtualpairprogrammers/HousePriceAnalysis.java
@@ -2,9 +2,14 @@ package com.virtualpairprogrammers;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.ml.evaluation.RegressionEvaluator;
 import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.param.ParamMap;
 import org.apache.spark.ml.regression.LinearRegression;
 import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.tuning.ParamGridBuilder;
+import org.apache.spark.ml.tuning.TrainValidationSplit;
+import org.apache.spark.ml.tuning.TrainValidationSplitModel;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -28,26 +33,42 @@ public class HousePriceAnalysis {
 
 		csvData.printSchema();
 
-		VectorAssembler vectorAssembler = new VectorAssembler();
-		vectorAssembler.setInputCols(new String[] { "bedrooms", "bathrooms", "sqft_living","sqft_lot", "floors", "grade" });
-		vectorAssembler.setOutputCol("features");
-		Dataset<Row> csvDataWithFeatures = vectorAssembler.transform(csvData);
-
-		Dataset<Row> modelInputData = csvDataWithFeatures.select("price", "features").withColumnRenamed("price","label");
+		VectorAssembler vectorAssembler = new VectorAssembler()
+				.setInputCols(new String[] { "bedrooms", "bathrooms", "sqft_living","sqft_lot", "floors", "grade" })
+				.setOutputCol("features");
+			
+		Dataset<Row> modelInputData = vectorAssembler.transform(csvData)
+				.select("price", "features")
+				.withColumnRenamed("price","label");
 		
 		//			modelInputData.show();
 		
-		Dataset<Row>[] trainingAndTestData = modelInputData.randomSplit(new double[] { 0.8, 0.2});
-		Dataset<Row> trainingData = trainingAndTestData[0];
-		Dataset<Row> testData = trainingAndTestData[1];
+		Dataset<Row>[] dataSplits = modelInputData.randomSplit(new double[] { 0.8, 0.2});
+		Dataset<Row> trainingAndTestData = dataSplits[0];
+		Dataset<Row> holdOutData = dataSplits[1];
 		
-		LinearRegressionModel model = new LinearRegression().fit(trainingData);
+		LinearRegression linearRegression = new LinearRegression();
 		
-		System.out.println("The training data r2 value is " + model.summary().r2() + " and the RMSE is " + model.summary().rootMeanSquaredError());
+		ParamGridBuilder paramGridBuilder = new ParamGridBuilder();
 		
-		model.transform(testData).show();
+		ParamMap[] paramMap = paramGridBuilder.addGrid(linearRegression.regParam(), new double[] { 0.01, 0.1, 0.5 })
+				.addGrid(linearRegression.elasticNetParam(), new double[] { 0, 0.5, 1 })
+				.build();
+		
+		TrainValidationSplit trainValidationSplit = new TrainValidationSplit()
+				.setEstimator(linearRegression)
+				.setEvaluator(new RegressionEvaluator().setMetricName("r2"))
+				.setEstimatorParamMaps(paramMap)
+				.setTrainRatio(0.8);
+		
+		TrainValidationSplitModel model = trainValidationSplit.fit(trainingAndTestData);
+		LinearRegressionModel lrModel = (LinearRegressionModel) model.bestModel();
+		
+		System.out.println("The training data r2 value is " + lrModel.summary().r2() + " and the RMSE is " + lrModel.summary().rootMeanSquaredError());
+		
+		//lrModel.transform(holdOutData).show();
 		//R2 should be close to 1 and RMSE should be smaller
-		System.out.println("The test data r2 value is " + model.evaluate(testData).r2() + " and the RMSE is " + model.evaluate(testData).rootMeanSquaredError());
+		System.out.println("The test data r2 value is " + lrModel.evaluate(holdOutData).r2() + " and the RMSE is " + lrModel.evaluate(holdOutData).rootMeanSquaredError());
 		
 		
 
