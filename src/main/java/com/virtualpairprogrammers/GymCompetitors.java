2dacc1063ad96038d5f372b10c3c875eaefddb3f
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-07-02 21:46:18 +0300

    OneHotEncoderEstimator for GymCompetitors

--- src/main/java/com/virtualpairprogrammers/GymCompetitors.java
+++ src/main/java/com/virtualpairprogrammers/GymCompetitors.java
@@ -2,9 +2,8 @@ package com.virtualpairprogrammers;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.ml.feature.VectorAssembler;
-import org.apache.spark.ml.regression.LinearRegression;
-import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.feature.OneHotEncoderEstimator;
+import org.apache.spark.ml.feature.StringIndexer;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -26,21 +25,32 @@ public class GymCompetitors {
 				.option("inferSchema", true)
 				.csv("src/main/resources/GymCompetition.csv");
 
-		csvData.printSchema();
-
-		VectorAssembler vectorAssembler = new VectorAssembler();
-		vectorAssembler.setInputCols(new String[] { "Age", "Height", "Weight" });
-		vectorAssembler.setOutputCol("features");
-		Dataset<Row> csvDataWithFeatures = vectorAssembler.transform(csvData);
-
-		Dataset<Row> modelInputData = csvDataWithFeatures.select("NoOfReps", "features").withColumnRenamed("NoOfReps","label");
+		//csvData.printSchema();
 		
-		modelInputData.show();
+		StringIndexer genderIndexer = new StringIndexer();
+		genderIndexer.setInputCol("Gender");
+		genderIndexer.setOutputCol("GenderIndex");
+		csvData = genderIndexer.fit(csvData).transform(csvData);
 		
-		LinearRegression linearRegression = new LinearRegression();
-		LinearRegressionModel model = linearRegression.fit(modelInputData);
-		System.out.println("The model has intercept "+model.intercept()+" and coefficients "+model.coefficients());
+		OneHotEncoderEstimator genderEncoder = new OneHotEncoderEstimator();
+		genderEncoder.setInputCols(new String[] {"GenderIndex"});
+		genderEncoder.setOutputCols(new String[] {"GenderVector"});
+		csvData = genderEncoder.fit(csvData).transform(csvData);
+		csvData.show();
 
-		model.transform(modelInputData).show();
+//		VectorAssembler vectorAssembler = new VectorAssembler();
+//		vectorAssembler.setInputCols(new String[] { "Age", "Height", "Weight" });
+//		vectorAssembler.setOutputCol("features");
+//		Dataset<Row> csvDataWithFeatures = vectorAssembler.transform(csvData);
+//
+//		Dataset<Row> modelInputData = csvDataWithFeatures.select("NoOfReps", "features").withColumnRenamed("NoOfReps","label");
+//		
+//		modelInputData.show();
+//		
+//		LinearRegression linearRegression = new LinearRegression();
+//		LinearRegressionModel model = linearRegression.fit(modelInputData);
+//		System.out.println("The model has intercept "+model.intercept()+" and coefficients "+model.coefficients());
+//
+//		model.transform(modelInputData).show();
 	}
 }
